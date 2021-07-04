3573c8582735a1295fa6c14f0aa1d10c3ab6a593
Gabriel Alcantara Bernardes <gabrielalcantaratp@gmail.com>
2021-07-04 18:51:21 -0300

    feat: adding routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { Home } from "../pages";
+import { Home, DrinksByCategory, Drink } from "../pages";
 
 const Routes: React.FC = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={Home} />
+      <Route path="/drinks" component={DrinksByCategory} />
+      <Route path="/drink/:idDrink" component={Drink} />
     </Switch>
-
   </BrowserRouter>
 );
 
