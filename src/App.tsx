10106640596aee081aca07c4b6084c1c3bf2705a
Gabriel Alcantara Bernardes <gabrielalcantaratp@gmail.com>
2021-07-04 18:49:45 -0300

    chore: adding redux to app provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { ThemeProvider } from "styled-components";
+import { Provider } from "react-redux";
+import store from "./store";
 import Routes from "./routes";
 import { theme, GlobalStyle } from "./themes";
 
 const App: React.FC = () => (
   <ThemeProvider theme={theme}>
-    <Routes />
+    <Provider store={store}>
+      <Routes />
+    </Provider>
     <GlobalStyle />
   </ThemeProvider>
 );
